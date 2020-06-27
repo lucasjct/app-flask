b05fa9e43d31557f179f2bf691c9ab41c4b7fc12
lucasjct <lucasjcteixeria@gmail.com>
2020-06-27 16:16:09 -0300

    add-config-files

--- delivery/delivery/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from delivery.app import create_app
-
-@pytest.fixture(scope="module")
-def app():
-    """Instace of Main flask app"""
-    return create_app()
--- delivery/delivery/tests/unit-test.py
+++
@@ -1,5 +0,0 @@
-def test_one_plus_one_two():
-    assert 1 + 1  == 2
-    
-def test_negative():
-    assert 1 - 4 == -3
