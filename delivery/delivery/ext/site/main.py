f33e9b06901faef55c8063ae9f1a2a3fcb19d188
lucasjct <lucasjcteixeria@gmail.com>
2020-07-05 02:46:31 -0300

    adding-templates

--- delivery/delivery/ext/site/main.py
+++ delivery/delivery/ext/site/main.py
@@ -7,7 +7,13 @@ bp = Blueprint("site", __name__)
 
 @bp.route("/")
 def index():
+    return render_template("index.html")
 
-    return render_template(
-        "index.html"
-    )
+
+@bp.route('/sobre')
+def about():
+   return render_template("about.html")
+
+@bp.route('/restaurantes')
+def restaurant():
+   return render_template('restaurants.html')
